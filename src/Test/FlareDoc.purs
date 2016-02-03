04227ed1e9628cecb306e47bdf4afd6fc3943197
sharkdp <davidpeter@web.de>
2016-02-03 23:11:23 +0100

    Move async code to FlareDoc module. Clean up.

--- src/Test/FlareDoc.purs
+++ src/Test/FlareDoc.purs
@@ -1,13 +1,19 @@
 module Test.FlareDoc
-  ( parseModuleJSON
-  , document'
-  , document
-  , module Test.FlareCheck
+  ( withPackage
+  , flareDoc'
+  , flareDoc
+  , module FCE
   ) where
 
 import Prelude
 
 import Control.Monad.Eff (Eff())
+
+import Control.Monad.Aff (launchAff)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION())
 
 import Data.Array (findIndex, index)
 import Data.Either (Either(..), either, isLeft, isRight)
@@ -17,24 +23,28 @@ import Data.Maybe (Maybe(..))
 import Data.StrMap as M
 import Data.Tuple (Tuple(..))
 
-import Data.Argonaut.Core
-import Data.Argonaut.Combinators
-import Data.Argonaut.Decode
+import Data.Argonaut.Core (Json, toObject)
+import Data.Argonaut.Combinators ((.?), (:=), (?>>=), (~>))
+import Data.Argonaut.Decode (class DecodeJson, decodeJson)
 
 import Text.Markdown.SlamDown
-import Text.Markdown.SlamDown.Parser
-import Text.Markdown.SlamDown.Pretty
+import Text.Markdown.SlamDown.Parser (parseMd)
+
+import Network.HTTP.Affjax (get, AJAX)
 
 import Signal.Channel (Chan())
 
 import DOM (DOM())
 
-import Test.FlareCheck
+import Test.FlareCheck hiding (flareDoc, flareDoc')
+import Test.FlareCheck hiding (flareDoc, flareDoc') as FCE
 import Test.FlareCheck as FC
 
+-- | Natural transformation from `Either a` to `Maybe`.
 fromEither :: forall a b. Either a b -> Maybe b
 fromEither = either (const Nothing) Just
 
+-- | A function declaration in a module.
 data Declaration = Declaration String (Maybe String)
 
 type ModuleRec =
@@ -42,11 +52,14 @@ type ModuleRec =
   , declarations :: Array Declaration
   }
 
+-- | A PureScript module.
 data Module = Module ModuleRec
 
+-- | Get the name of a PureScript module.
 name :: Module -> String
 name (Module mod) = mod.name
 
+-- | A PureScript package.
 data Package = Package (Array Module)
 
 instance decodePackage :: DecodeJson Package where
@@ -69,8 +82,10 @@ instance decodeDeclaration :: DecodeJson Declaration where
       comments <- M.lookup "comments" obj ?>>= "comments" >>= decodeJson
       return $ Declaration title comments
 
+-- | The full documentation of a PureScript package.
 type Documentation = M.StrMap (M.StrMap (Maybe String))
 
+-- | Parse the `psc-publish`-generated JSON format.
 parseModuleJSON :: Json -> Either String Documentation
 parseModuleJSON json = do
   (Package modules) <- decodeJson json
@@ -83,7 +98,7 @@ parseModuleJSON json = do
 
     declTuple (Declaration title comment) = Tuple title comment
 
--- Premature Markdown Render (TODO!)
+-- Very premature Markdown Render (TODO!)
 render :: SlamDown -> String
 render (SlamDown list) = foldMap block list
   where
@@ -97,14 +112,28 @@ render (SlamDown list) = foldMap block list
     inline (Code _ str) = "<code>" <> str <> "</code>"
     inline SoftBreak = " "
 
-document' :: forall t e. (Interactive t)
+-- | Parse a package description and run the interactive documentation.
+withPackage :: forall eff
+             . String
+            -> (Documentation -> Eff (ajax :: AJAX, console :: CONSOLE, chan :: Chan, dom :: DOM) Unit)
+            -> Eff (ajax :: AJAX, err :: EXCEPTION, console :: CONSOLE, chan :: Chan, dom :: DOM) Unit
+withPackage packageDescription run = launchAff do
+  r <- get packageDescription
+  case parseModuleJSON r.response of
+    Left err -> do
+      liftEff (log err)
+    Right documentation -> do
+      liftEff (run documentation)
+
+-- | Like `flareDoc`, but the HTML element can be specified.
+flareDoc' :: forall t e. (Interactive t)
           => ElementId
           -> Documentation
           -> String
           -> String
           -> t
           -> Eff (chan :: Chan, dom :: DOM | e) Unit
-document' parentId docMap moduleName functionName x = do
+flareDoc' parentId docMap moduleName functionName x = do
   let docString = do
         decls <- M.lookup moduleName docMap
         mcomment <- M.lookup functionName decls
@@ -115,10 +144,12 @@ document' parentId docMap moduleName functionName x = do
       title = moduleName ++ "." ++ functionName
   FC.flareDoc' parentId title docString x
 
-document :: forall t e. (Interactive t)
+-- | Add an interactive documentation entry. The `String` arguments specify the
+-- | module name and the function name, respectively.
+flareDoc :: forall t e. (Interactive t)
          => Documentation
          -> String
          -> String
          -> t
          -> Eff (chan :: Chan, dom :: DOM | e) Unit
-document = document' "flaredoc"
+flareDoc = flareDoc' "flaredoc"
--- test/Main.purs
+++ test/Main.purs
@@ -1,77 +1,58 @@
 module Test.Main where
 
 import Prelude
-
-import Control.Monad.Aff (launchAff)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Exception (EXCEPTION())
-
 import Data.Array
-import Data.Either (Either(..))
+import Data.Generic (class Generic)
 import Data.Int (even)
 import Data.Maybe (Maybe())
-import Data.Generic (class Generic)
+import Test.FlareDoc (class Interactive, withPackage, flareDoc)
 
-import Network.HTTP.Affjax (get, AJAX)
-import DOM (DOM())
-import Signal.Channel (Chan())
-
-import Test.FlareDoc (class Interactive, document, parseModuleJSON)
 
 newtype UnconsRecord = UnconsRecord { head :: Int, tail :: Array Int }
 
 derive instance genericUnconsRecord :: Generic UnconsRecord
 
-main :: Eff (chan :: Chan, dom :: DOM, err :: EXCEPTION, ajax :: AJAX, console :: CONSOLE) Unit
-main = launchAff do
-  r <- get "purescript-arrays.json"
-  let dictionary = parseModuleJSON r.response
-  case dictionary of
-    Left err -> do
-      liftEff $ (log err :: Eff _ Unit)
-      return unit
-    Right dict -> do
-      let doc :: forall t. Interactive t => _ -> t -> _
-          doc = document dict "Data.Array"
-      liftEff do
-        doc "range" range
-        doc "replicate" (replicate :: Int -> String -> _)
-        doc "uncons" (map UnconsRecord <<< uncons :: Array Int -> _)
-        doc "group" (group :: Array Int -> _)
-        doc "group'" (group' :: Array Int -> _)
-        doc "null" (null :: Array Int -> _)
-        doc "length" (length :: Array Int -> _)
-        doc "cons" (cons :: Int -> _)
-        doc "snoc" (snoc :: _ -> Int -> _)
-        doc "insert" (insert :: Int -> _)
-        doc "head" (head :: Array Int -> _)
-        doc "last" (last :: Array Int -> _)
-        doc "tail" (tail :: Array Int -> _)
-        doc "init" (init :: Array Int -> _)
-        doc "index" (index :: Array String -> _)
-        doc "elemIndex" (elemIndex :: String -> _)
-        doc "elemLastIndex" (elemLastIndex :: String -> _)
-        doc "findIndex even" (findIndex even)
-        doc "findLastIndex even" (findLastIndex even)
-        doc "insertAt" (insertAt :: _ -> String -> _)
-        doc "deleteAt" (deleteAt :: _ -> Array String -> _)
-        doc "updateAt" (updateAt :: _ -> String -> _)
-        doc "modifyAt" $ \i -> modifyAt i (* 10)
-        doc "reverse" (reverse :: Array Int -> _)
-        doc "concatMap (\\n -> range 0 n)" (concatMap (\n -> range 0 n))
-        doc "catMaybes" $ \x y z -> catMaybes [(x :: Maybe String), y, z]
-        doc "sort" (sort :: Array Int -> _)
-        doc "slice" (slice :: Int -> Int -> Array String -> _)
-        doc "singleton" (singleton :: Int -> _)
-        doc "take" (take :: Int -> Array Int -> _)
-        doc "takeWhile even" (takeWhile even)
-        doc "drop" (drop :: Int -> Array Int -> _)
-        doc "dropWhile even" (dropWhile even)
-        doc "nub" (nub :: Array Int -> _)
-        doc "union" (union :: Array Int -> _)
-        doc "delete" (delete :: Int -> _)
-        doc "(\\\\)" ((\\) :: Array Int -> _)
-        doc "intersect" (intersect :: Array Int -> _)
-        doc "zip" (zip :: Array Int -> Array String -> _)
+main = do
+  withPackage "purescript-arrays.json" $ \dict -> do
+    let doc :: forall t. Interactive t => String -> t -> _
+        doc = flareDoc dict "Data.Array"
+
+    doc "range" range
+    doc "replicate" (replicate :: Int -> String -> _)
+    doc "uncons" (map UnconsRecord <<< uncons :: Array Int -> _)
+    doc "group" (group :: Array Int -> _)
+    doc "group'" (group' :: Array Int -> _)
+    doc "null" (null :: Array Int -> _)
+    doc "length" (length :: Array Int -> _)
+    doc "cons" (cons :: Int -> _)
+    doc "snoc" (snoc :: _ -> Int -> _)
+    doc "insert" (insert :: Int -> _)
+    doc "head" (head :: Array Int -> _)
+    doc "last" (last :: Array Int -> _)
+    doc "tail" (tail :: Array Int -> _)
+    doc "init" (init :: Array Int -> _)
+    doc "index" (index :: Array String -> _)
+    doc "elemIndex" (elemIndex :: String -> _)
+    doc "elemLastIndex" (elemLastIndex :: String -> _)
+    doc "findIndex even" (findIndex even)
+    doc "findLastIndex even" (findLastIndex even)
+    doc "insertAt" (insertAt :: _ -> String -> _)
+    doc "deleteAt" (deleteAt :: _ -> Array String -> _)
+    doc "updateAt" (updateAt :: _ -> String -> _)
+    doc "modifyAt" $ \i -> modifyAt i (* 10)
+    doc "reverse" (reverse :: Array Int -> _)
+    doc "concatMap (\\n -> range 0 n)" (concatMap (\n -> range 0 n))
+    doc "catMaybes" $ \x y z -> catMaybes [(x :: Maybe String), y, z]
+    doc "sort" (sort :: Array Int -> _)
+    doc "slice" (slice :: Int -> Int -> Array String -> _)
+    doc "singleton" (singleton :: Int -> _)
+    doc "take" (take :: Int -> Array Int -> _)
+    doc "takeWhile even" (takeWhile even)
+    doc "drop" (drop :: Int -> Array Int -> _)
+    doc "dropWhile even" (dropWhile even)
+    doc "nub" (nub :: Array Int -> _)
+    doc "union" (union :: Array Int -> _)
+    doc "delete" (delete :: Int -> _)
+    doc "(\\\\)" ((\\) :: Array Int -> _)
+    doc "intersect" (intersect :: Array Int -> _)
+    doc "zip" (zip :: Array Int -> Array String -> _)
